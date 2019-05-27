b5f3ba2ca820e534dd1c2d51e8af9cc47377be5a
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-27 19:53:54 -0300

    ENH: add page

---
+++ docs/precache-manifest.de35006dc293926a64afb295c709c366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c3a68c4471176040f25",
+    "url": "/static/js/main.3c3a68c4.chunk.js"
+  },
+  {
+    "revision": "4ae461615a6f0aa4325a",
+    "url": "/static/js/1.4ae46161.chunk.js"
+  },
+  {
+    "revision": "4ae461615a6f0aa4325a",
+    "url": "/static/css/1.2e3ceb3f.chunk.css"
+  },
+  {
+    "revision": "c5eecc7a86e70c168266dd27e06ec54b",
+    "url": "/index.html"
+  }
+];
