a8f2827238b67a3f0e74463498ce9b0cee45670b
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-27 20:13:48 -0300

    deploy pages

--- src/presentation.js
+++ src/presentation.js
@@ -33,7 +33,7 @@ const theme = createTheme(
   }
 );
 
-const LOGO = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K';
+const LOGO = 'https://cdn.svgporn.com/logos/react.svg';
 const photoURL = "https://avatars1.githubusercontent.com/u/4452113?s=460&v=4";
 
 export default function Presentation(props) {
