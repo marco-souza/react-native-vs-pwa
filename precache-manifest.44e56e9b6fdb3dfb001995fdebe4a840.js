819b47c5ea563da93b00b84a339de7af3d102ca5
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-28 23:20:09 -0300

    Updates

---
+++ precache-manifest.44e56e9b6fdb3dfb001995fdebe4a840.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cec5fbdaeea4e70e4516",
+    "url": "/static/js/main.cec5fbda.chunk.js"
+  },
+  {
+    "revision": "7b630c291ebc759a07b8",
+    "url": "/static/js/1.7b630c29.chunk.js"
+  },
+  {
+    "revision": "7b630c291ebc759a07b8",
+    "url": "/static/css/1.9ee5fafa.chunk.css"
+  },
+  {
+    "revision": "e93e0961e022dfcb3aa88559a96b19a7",
+    "url": "/index.html"
+  }
+];
