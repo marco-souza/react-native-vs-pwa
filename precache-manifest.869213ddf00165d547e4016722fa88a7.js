e8dc8ba82b107a0ebdfbba103386225b5b560aa0
Marco Antônio <ma.souza.junior@gmail.com>
2019-05-28 23:23:19 -0300

    Updates

---
+++ precache-manifest.869213ddf00165d547e4016722fa88a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f479794329974dfd611",
+    "url": "/react-native-vs-pwa/static/js/runtime~main.3f479794.js"
+  },
+  {
+    "revision": "cec5fbdaeea4e70e4516",
+    "url": "/react-native-vs-pwa/static/js/main.cec5fbda.chunk.js"
+  },
+  {
+    "revision": "7b630c291ebc759a07b8",
+    "url": "/react-native-vs-pwa/static/js/1.7b630c29.chunk.js"
+  },
+  {
+    "revision": "7b630c291ebc759a07b8",
+    "url": "/react-native-vs-pwa/static/css/1.9ee5fafa.chunk.css"
+  },
+  {
+    "revision": "d08a5ac880734e6258530613aa1dc0b4",
+    "url": "/react-native-vs-pwa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cbd8852138b1fbbbde8323657712514.js"
+  "/react-native-vs-pwa/precache-manifest.869213ddf00165d547e4016722fa88a7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-native-vs-pwa/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
